8615bdb0869c92a875e56c2cb70efcbbe4162653
Syrus Akbary <dimension.net@gmail.com>
2011-04-26 22:24:39 +0200

    Primera versión

---
+++ compress.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+zip  tuentify ./images/activated.png ./images/deactivated.png ./images/icon16.png ./images/icon32.png ./images/icon48.png ./images/icon128.png ./background.html ./manifest.json

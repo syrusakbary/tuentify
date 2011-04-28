48eab3b2204754634f2bc1fce754219fc2bdd2df
Syrus Akbary <dimension.net@gmail.com>
2011-04-28 03:07:00 +0200

    Corregidos pequeños cambios

--- compress.sh
+++ compress.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-zip  tuentify ./images/activated.png ./images/deactivated.png ./images/icon16.png ./images/icon32.png ./images/icon48.png ./images/icon128.png ./background.html ./manifest.json
+zip  tuentify ./images/activated.png ./images/deactivated.png ./images/icon16.png ./images/icon32.png ./images/icon48.png ./images/icon128.png ./background.html ./popup.html ./manifest.json ./css/* ./js/*
--- js/popup.js
+++ js/popup.js
@@ -1,49 +1,8 @@
-function updateData (data) {
-	//d.unread_friend_messages.count>0
-}
 $(document).ready(function () {
 	var bg = chrome.extension.getBackgroundPage();
-	/*var more = {
-		unread_friend_messages: {
-			singular : "{count} mensaje privado",
-			plural: "{count} mensajes privados",
-			tmpl : ""
-		},
-		unread_spam_messages: {
-			singular : "{count} mensaje de un desconocido",
-			plural: "{count} mensajes de desconocidos",
-		},
-		new_profile_wall_posts:{
-			singular : "{count} comentario en tu tablón",
-			plural: "{count} comentarios en tu tablón",
-		},
-		new_friend_requests:{
-			singular : "{count} petición de amigo",
-			plural: "{count} peticiones de amigos",
-		},
-		accepted_friend_requests:{
-			singular : "{count} petición de amigo",
-			plural: "{count} peticiones de amigos",
-		},
-		new_photo_wall_posts:{
-		},
-		new_tagged_photos:{
-		},
-		new_event_invitations:{
-		},
-		new_group_page_invitations:{
-		},
-		group_admin_promotions:{
-		},
-		group_member_promotions:{
-		},
-		new_profile_wall_comments:{
-		}
-	} */
 	$('a').live('click',function(){chrome.tabs.create({url:$(this).attr('href')});});
 	
 	if (bg.logged) {
-		//updateData(bg.data);
 		if (bg.notifies==0) {
 			$( "#notifications" ).hide(0);
 			$( "#no-notifications" ).show(0);
@@ -53,7 +12,6 @@ $(document).ready(function () {
 			$( "#notifications" ).show(0);
 			$( "#no-notifications").hide(0);
 			$.each(["unread_friend_messages","unread_spam_messages","new_profile_wall_comments","new_profile_wall_posts","new_event_invitations","new_tagged_photos","new_photo_wall_posts","new_group_page_invitations",/*"group_admin_promotions","group_member_promotions",*/"new_friend_requests","accepted_friend_requests"],function(index,key) {
-				//tmpl.push({count:value.count,ta:'#'+key+'-template'});
 				try {
 				var t = '#'+key+'-template';
 				value = bg.data[key];
@@ -65,10 +23,6 @@ $(document).ready(function () {
 				catch (e) {}
 			});
 		}
-		//alert(bg.data.new_tagged_photos);
-		//$.tmpl( '<li>a{{tmpl "#new_tagged_photos-template" }}</li>',bg.data.new_tagged_photos).appendTo( "#notifications" )
-		
-		//$.tmpl( "<li>${ta}{{tmpl $ta }}</li>", tmpl).appendTo( "#target" );
 	}
 	else {
 		chrome.tabs.create({url:'http://www.tuenti.com'});
